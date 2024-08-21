f06554db5657b3f68dbfc0eb5fbc3cdcce87c9de
LuigiDiBella <lykos.omega@gmail.com>
2024-08-21 21:07:30 +0200

    [add/edit] difficulty, setMaxSquares(), createBombs(), createGrid(), showBombs(), reset()

--- js/script.js
+++ js/script.js
@@ -1,44 +1,87 @@
 const grid = document.querySelector('.grid');
 const playButton = document.querySelector('.play');
 
-const totalCells = 100;
+const difficultyArray = [100, 81, 49];
 const totalBombs = 16;
 const bombList = [];
-
-while (bombList.length < totalBombs) {
-  const number = Math.floor(Math.random() * totalCells) + 1;
-  if (!bombList.includes(number)) bombList.push(number);
-}
-
-console.log(bombList);
 
 playButton.addEventListener('click', play);
 
 function play(){
+  
+  const difficulty = document.getElementById('difficulty').value;
+  
+  if(difficulty <= 2) {
+    if(playButton.textContent === 'Play') {
+      playButton.textContent = 'Reset';
+      playButton.className = 'btn btn-outline-danger';
+    }
+    reset();
+    setMaxSquares(difficulty);
+    createBombs();
+    console.log(bombList);
+    createGrid();
+  }
+}
+
+// Funzione per calcolare numero celle
+function setMaxSquares(difficultyValue) {
+  maxSquares = difficultyArray[difficultyValue];
+}
+
+// Funzione per creare le bombe
+function createBombs() {
+  bombList.splice(0); // Svuota la lista delle bombe
+  while (bombList.length < totalBombs) {
+    const number = Math.floor(Math.random() * maxSquares) + 1;
+    if (!bombList.includes(number)) bombList.push(number);
+  }
+  bombList.sort((a, b) => a - b); // Ordina i numeri in ordine crescente
+}
+
+// Funzione per creare la griglia
+function createGrid() {
   grid.innerHTML = '';
-  for (let i = 1; i <= totalCells; i++){
+  grid.className = 'grid' + maxSquares;
+  for (let i = 1; i <= maxSquares; i++){
     const cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.innerHTML += `<div class="num">${[i]}</div>`;
-
+    
+    // Aggiungi data-id per identificare la cella
+    cell.setAttribute('data-id', i);
+    
     cell.addEventListener('click', function(){
       if (cell.classList.contains('cell-clicked')) return;
-
-      /* cell.classList.add('cell-clicked'); */
-
-      /* let numElement = this.querySelector('.num');
-      let num = numElement.textContent;
-      console.log(num); */
-
-      if (bombList.includes(i)) {
+      
+      if (bombList.includes(i)){
         cell.classList.add('cell-bomb');
+        cell.innerHTML += `<i class="fa-solid fa-land-mine-on" style="color: black;"></i>`;
         alert('Hai Perso');
-        location.reload();
+        showBombs();
       } else {
         cell.classList.add('cell-clicked');
       }
     });
-
     grid.appendChild(cell);
   }
+}
+
+// Funzione per mostrare tutte le bombe
+function showBombs() {
+  const allCell = document.querySelectorAll('.cell');
+
+  for (let i = 0; i < allCell.length; i++) {
+    const cellID = parseInt(allCell[i].getAttribute('data-id'));
+    if (bombList.includes(cellID)) {
+      allCell[i].classList.add('cell-bomb');
+      allCell[i].innerHTML = '<i class="fa-solid fa-land-mine-on" style="color: black;"></i>';
+    }
+  }
+}
+
+
+// Funzione di reset della griglia
+function reset() {
+  grid.innerHTML = '';
+  bombList.splice(0);
 }
